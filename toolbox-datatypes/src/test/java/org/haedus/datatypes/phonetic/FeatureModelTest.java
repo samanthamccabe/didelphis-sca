53592768066125ee772aa4b1a142942b3c363171
SFM McCabe <fiona.morrigan@gmail.com>
2014-11-16 22:27:00 -0500

    Added gitignore to root

--- toolbox-datatypes/src/test/java/org/haedus/datatypes/phonetic/FeatureModelTest.java
+++ toolbox-datatypes/src/test/java/org/haedus/datatypes/phonetic/FeatureModelTest.java
@@ -16,6 +16,7 @@
 
 package org.haedus.datatypes.phonetic;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,59 +35,57 @@ import static org.junit.Assert.assertEquals;
 public class FeatureModelTest {
 	private static final transient Logger LOGGER = LoggerFactory.getLogger(FeatureModelTest.class);
 
+	private static final List<Double> G_FEATURES  = new ArrayList<Double>();
+	private static final List<Double> GH_FEATURES = new ArrayList<Double>();
+	private static final double       NAN         = Double.NaN;
+
+	@BeforeClass
+	public static void init() {
+		Collections.addAll(G_FEATURES,  0.0, -1.0, NAN, 1.0, NAN, NAN, NAN, NAN, NAN, NAN, 1.0, -1.0, 1.0, NAN, NAN, NAN, 0.0, 0.0);
+		Collections.addAll(GH_FEATURES, 0.0, -1.0, 1.0, 1.0, NAN, NAN, NAN, NAN, NAN, NAN, 1.0, -1.0, 1.0, NAN, NAN, NAN, 0.0, 0.0);
+	}
+
 	@Test
 	public void testConstructor01() throws Exception {
 		Resource resource = new ClassPathResource("featuremodel");
 		FeatureModel model = new FeatureModel(resource.getFile());
 
-		List<Double> features = new ArrayList<Double>();
-		Collections.addAll(features,
-				3d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, -1d, -1d, 1d, 2d, 1d, 0d, 0d, 0d, 0d);
-
 		Segment received = model.get("g");
-		Segment expected = new Segment("g", features);
+		Segment expected = new Segment("g", G_FEATURES);
 
 		assertEquals(expected, received);
 	}
 
 	@Test
 	public void testGetStringFromFeatures01() throws Exception {
-		List<Double> expected = new ArrayList<Double>();
-		Collections.addAll(expected,
-				3d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, -1d, -1d, 1d, 2d, 1d, 0d, 0d, 0d, 0d);
-
 		Resource resource = new ClassPathResource("featuremodel");
 		FeatureModel model = new FeatureModel(resource.getFile());
 
-		String bestSymbol = model.getBestSymbol(expected);
+		String bestSymbol = model.getBestSymbol(G_FEATURES);
 		LOGGER.info(bestSymbol);
 		assertEquals("g", bestSymbol);
 	}
 
 	@Test
-		 public void testGetStringFromFeatures02() throws Exception {
-		List<Double> expected = new ArrayList<Double>();
-		Collections.addAll(expected,
-				3d, 0d, 1d, 0d, 0d, 0d, 0d, 0d, -1d, -1d, 1d, 2d, 1d, 0d, 0d, 0d, 0d);
-
+	public void testGetStringFromFeatures02() throws Exception {
 		Resource resource = new ClassPathResource("featuremodel");
 		FeatureModel model = new FeatureModel(resource.getFile());
 
-		String bestSymbol = model.getBestSymbol(expected);
+		String bestSymbol = model.getBestSymbol(GH_FEATURES);
 		assertEquals("gʰ", bestSymbol);
 	}
 
 	@Test
 	public void testGetStringFromFeatures03() throws Exception {
-//		List<Double> expected = new ArrayList<Double>();
-//		Collections.addAll(expected,
-//				3d, 0d, 1d, 0d, 0d, 0d, 0d, 0d, -1d, -1d, 1d, 2d, 1d, 0d, 0d, 2d, 0d);
+		//		List<Double> expected = new ArrayList<Double>();
+		//		Collections.addAll(expected,
+		//				3d, 0d, 1d, 0d, 0d, 0d, 0d, 0d, -1d, -1d, 1d, 2d, 1d, 0d, 0d, 2d, 0d);
 		Resource resource = new ClassPathResource("featuremodel");
 		FeatureModel model = new FeatureModel(resource.getFile());
 
 		String symbol = "gʼ";
-//		List<Double> expected = model.compile(symbol).getFeatures();
-//		String bestSymbol = model.getBestSymbol(expected);
-//		assertEquals(symbol, bestSymbol);
+		//		List<Double> expected = model.compile(symbol).getFeatures();
+		//		String bestSymbol = model.getBestSymbol(expected);
+		//		assertEquals(symbol, bestSymbol);
 	}
 }
