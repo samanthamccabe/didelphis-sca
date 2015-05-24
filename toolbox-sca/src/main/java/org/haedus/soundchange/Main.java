397245c123220fd917135e011d8ba849fb866eab
Samantha F Mccabe <fiona.morrigan@gmail.com>
2015-05-24 17:57:14 -0400

    [toolbox-sca-40] Support Feature arrays in Rule transform

--- toolbox-sca/src/main/java/org/haedus/soundchange/Main.java
+++ toolbox-sca/src/main/java/org/haedus/soundchange/Main.java
@@ -33,12 +33,12 @@ import java.io.IOException;
  */
 public class Main {
 	private static final transient Logger LOGGER = LoggerFactory.getLogger(Main.class);
+	public static final String UTF_8 = "UTF-8";
 
 	private Main() {
 	}
 
-	public static void main(String[] args) throws IOException, ParseException
-	{
+	public static void main(String[] args) throws IOException, ParseException {
 		double startTime = System.nanoTime();
 		// 1) toolbox example.rule # loads rule; all segmentation/normalization is controlled internally
 		// 2) toolbox -b [-m(S|D|C)] input.lex basic.rule output.lex
@@ -48,11 +48,6 @@ public class Main {
 
 		if (args.length == 0) {
 			throw new IllegalArgumentException("No arguments were provided!");
-		} else if (args.length == 1) {
-			// If there is a single argument, it must be a rule
-			String rules = FileUtils.readFileToString(new File(args[0]), "UTF-8");
-			SoundChangeScript standardScript = new StandardScript(rules);
-			standardScript.process();
 		} else if (args[0].equals("-b")) {
 			String lexiconPath;
 			String rulesPath;
@@ -61,18 +56,10 @@ public class Main {
 			FormatterMode mode;
 			if (args[1].startsWith("-m")) {
 				lexiconPath = args[2];
-				rulesPath   = args[3];
-				outputPath  = args[4];
+				rulesPath = args[3];
+				outputPath = args[4];
 				String modeFlag = args[1].substring(2);
-				if (modeFlag.equals("S")) {
-					mode = FormatterMode.INTELLIGENT;
-				} else if (modeFlag.equals("D")) {
-					mode = FormatterMode.DECOMPOSITION;
-				} else if (modeFlag.equals("C")) {
-					mode = FormatterMode.COMPOSITION;
-				} else {
-					throw new IllegalArgumentException("Unsupported or unknown format flag '"+modeFlag+"' was provided!");
-				}
+				mode = getFormatterMode(modeFlag);
 			} else {
 				lexiconPath = args[1];
 				rulesPath   = args[2];
@@ -80,23 +67,45 @@ public class Main {
 				mode = FormatterMode.NONE;
 			}
 
-			String lexicon = FileUtils.readFileToString(new File(lexiconPath), "UTF-8");
-			String rules   = FileUtils.readFileToString(new File(rulesPath),   "UTF-8");
+			String lexicon = FileUtils.readFileToString(new File(lexiconPath), UTF_8);
+			String rules   = FileUtils.readFileToString(new File(rulesPath),   UTF_8);
 
 			SoundChangeScript sca = new BasicScript(rules, lexicon, mode);
 			sca.process();
 
 			Lexicon outputLexicon = sca.getLexicon(BasicScript.DEFAULT_LEXICON);
-			FileUtils.write(new File(outputPath), "UTF-8", outputLexicon.toString());
+			FileUtils.write(new File(outputPath), UTF_8, outputLexicon.toString());
 
+//		} else {
+//			throw new IllegalArgumentException("An improper number of arguments were provided!\n" +
+//					"If running in 'basic' mode, ensure that the first parameter is 'b', followed by INPUT, RULE, and OUTPUT file paths.\n" +
+//					"If running in 'standard' mode, only provide the path of a rule file.");
+//		}
 		} else {
-			throw new IllegalArgumentException("An improper number of arguments were provided!\n" +
-					"If running in 'basic' mode, ensure that the first parameter is 'b', followed by INPUT, RULE, and OUTPUT file paths.\n" +
-					"If running in 'standard' mode, only provide the path of a rule file.");
+			for (String arg : args) {
+				File ruleFile = new File(arg);
+				String rules = FileUtils.readFileToString(ruleFile, UTF_8);
+				SoundChangeScript standardScript = new StandardScript(rules);
+				standardScript.process();
+			}
 		}
 
 		double elapsedTime = System.nanoTime() - startTime;
 		double time = elapsedTime / StrictMath.pow(10.0,9.0);
 		LOGGER.info("Finished in {} seconds", time);
+	}
+
+	private static FormatterMode getFormatterMode(String modeFlag) {
+		FormatterMode mode;
+		if (modeFlag.equals("S")) {
+			mode = FormatterMode.INTELLIGENT;
+		} else if (modeFlag.equals("D")) {
+			mode = FormatterMode.DECOMPOSITION;
+		} else if (modeFlag.equals("C")) {
+			mode = FormatterMode.COMPOSITION;
+		} else {
+			throw new IllegalArgumentException("Unsupported or unknown format flag '"+modeFlag+"' was provided!");
+		}
+		return mode;
 	}
 }
