898583580c2863b0bef202bec0a7770dd2dbfde2
Samantha F Mccabe <fiona.morrigan@gmail.com>
2016-11-05 19:39:38 -0400

    [toolbox-sca-118] Add Instrumentation to StandardScript

--- toolbox-sca/src/main/java/org/didelphis/soundchange/ErrorLogger.java
+++ toolbox-sca/src/main/java/org/didelphis/soundchange/ErrorLogger.java
@@ -7,13 +7,14 @@
 package org.didelphis.soundchange;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
  * Samantha Fiona Morrigan McCabe
  * Created: 11/5/2016
  */
-public class ErrorLogger {
+public class ErrorLogger implements Iterable<ErrorLogger.Error> {
 	
 	private final List<Error> errors;
 	
@@ -28,7 +29,16 @@ public class ErrorLogger {
 	public void clear() {
 		errors.clear();
 	}
-	
+
+	public boolean isEmpty() {
+		return errors.isEmpty();
+	}
+
+	@Override
+	public Iterator<Error> iterator() {
+		return errors.iterator();
+	}
+
 	public static class Error {
 		private final String script;
 		private final int line;
@@ -41,5 +51,36 @@ public class ErrorLogger {
 			this.data = data;
 			this.exception = exception;
 		}
+
+		@Override
+		public String toString() {
+			return new StringBuilder()
+					.append(line)
+					.append(script)
+					.toString();
+		}
+
+		public String getScript() {
+			return script;
+		}
+
+		public int getLine() {
+			return line;
+		}
+
+		public String getData() {
+			return data;
+		}
+
+		public Exception getException() {
+			return exception;
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "ErrorLogger{" +
+				"errors=" + errors +
+				'}';
 	}
 }
