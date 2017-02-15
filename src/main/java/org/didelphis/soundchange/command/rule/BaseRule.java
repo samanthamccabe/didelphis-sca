797decc51520b96c6f641698e30ff5f806638a7e
Samantha F Mccabe <fiona.morrigan@gmail.com>
2017-02-14 23:29:19 -0500

    [didelphis-sca-130] Update Project and Documentation for Java 8 Transition

--- src/main/java/org/didelphis/soundchange/command/rule/BaseRule.java
+++ src/main/java/org/didelphis/soundchange/command/rule/BaseRule.java
@@ -12,6 +12,7 @@ import org.didelphis.common.language.phonetic.SequenceFactory;
 import org.didelphis.common.language.phonetic.features.FeatureArray;
 import org.didelphis.common.language.phonetic.features.SparseFeatureArray;
 import org.didelphis.common.language.phonetic.model.FeatureModel;
+import org.didelphis.common.language.phonetic.model.FeatureSpecification;
 import org.didelphis.common.language.phonetic.sequences.Sequence;
 import org.didelphis.soundchange.Condition;
 import org.slf4j.Logger;
@@ -37,7 +38,7 @@ public class BaseRule implements Rule {
 
 	private static final transient Logger LOGGER = LoggerFactory.getLogger(BaseRule.class);
 	
-	private static final Pattern BACKREFERENCE = Pattern.compile("\\$([^\\$]*)(\\d+)");
+	private static final Pattern BACKREFERENCE = Pattern.compile("\\$([^$]*)(\\d+)");
 	private static final Pattern NOT_PATTERN   = Pattern.compile("\\s*not\\s*", Pattern.CASE_INSENSITIVE);
 	private static final Pattern OR_PATTERN    = Pattern.compile("\\s*or\\s*", Pattern.CASE_INSENSITIVE);
 	
@@ -60,6 +61,79 @@ public class BaseRule implements Rule {
 		exceptions = new ArrayList<>();
 		conditions = new ArrayList<>();
 		parseRule();
+	}
+
+	@Override
+	public void run() {
+		// Does nothing
+	}
+
+	@Override
+	public Sequence apply(Sequence sequence) {
+		// Step through the word to see if the rule might apply, i.e. if the
+		// source pattern can be found
+		for (int index = 0; index < sequence.size(); ) {
+			index = applyAtIndex(sequence, index);
+		}
+		return sequence;
+	}
+
+	@Override
+	public int applyAtIndex(Sequence sequence, int index) {
+		int startIndex = index;
+		boolean unmatched = true;
+		// Check each source pattern
+		for (Map.Entry<Sequence, Sequence> entry : transform.entrySet()) {
+			Sequence source = entry.getKey();
+			Sequence target = entry.getValue();
+			
+			if (startIndex < sequence.size()) {
+				ruleMatcher.reset();
+				
+				int testIndex = startIndex;
+				// Step through the current source pattern
+				testIndex = matchSource(sequence, source, testIndex);
+
+				// This is checked second for a good reason: it may not be
+				// possible to know in advance what is the length of the
+				// matching initial until it's been evaluated, esp. in the
+				// case of a variable whose elements are allowed to have a
+				// length greater than 1. This is allowed because it is
+				// possible, or even likely, that a language might have a
+				// set of multi-segment clusters which still pattern
+				// together, or which are part of conditioning environments.
+				if (testIndex >= 0 && conditionsMatch(sequence, startIndex, testIndex)) {
+					// Now at this point, if everything worked, we can
+					Sequence removed = (startIndex < testIndex) 
+						  ? sequence.remove(startIndex, testIndex) 
+						  : factory.getNewSequence();
+					Sequence replacement = getReplacement(removed, target);
+					if (!replacement.isEmpty()) {
+						sequence.insert(replacement, startIndex);
+					}
+					startIndex = testIndex + replacement.size() - removed.size();
+					unmatched = false;
+				}
+			}
+		}
+		return unmatched ? startIndex+1 : startIndex;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(ruleText, conditions, exceptions, factory, transform);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {return true;}
+		if (!(o instanceof BaseRule)) {return false;}
+		BaseRule baseRule = (BaseRule) o;
+		return Objects.equals(ruleText, baseRule.ruleText) 
+				       && Objects.equals(conditions, baseRule.conditions) 
+				       && Objects.equals(exceptions, baseRule.exceptions)
+				       && Objects.equals(factory, baseRule.factory)
+				       && Objects.equals(transform, baseRule.transform);
 	}
 
 	@Override
@@ -89,106 +163,85 @@ public class BaseRule implements Rule {
 		return sb.toString();
 	}
 
-	@Override
-	public void run() {
-		// Does nothing
-	}
+	private static List<String> parseToList(String source) {
+		List<String> list = new ArrayList<>();
+		int start = 0;
+		int end   = 0;
 
-	@Override
-	public Sequence apply(Sequence sequence) {
-		// Step through the word to see if the rule might apply, i.e. if the
-		// source pattern can be found
-		for (int index = 0; index < sequence.size(); ) {
-			index = applyAtIndex(sequence, index);
-		}
-		return sequence;
-	}
-
-	@Override
-	public int applyAtIndex(Sequence sequence, int index) {
-		int startIndex = index;
-		boolean unmatched = true;
-		// Check each source pattern
-		for (Map.Entry<Sequence, Sequence> entry : transform.entrySet()) {
-			Sequence source = entry.getKey();
-			Sequence target = entry.getValue();
-
-			if (index < sequence.size()) {
-
-				ruleMatcher.reset();
-				
-				// Step through the source pattern
-				int testIndex = index;
-				int refIndex = 1;
-				for (int i = 0; i < source.size() && testIndex >= 0; i++) {
-					Sequence subSequence = sequence.subsequence(testIndex);
-					Segment segment = source.get(i);
-
-					// Source symbol is a variable
-					String symbol = segment.getSymbol();
-					if (factory.hasVariable(symbol)) {
-						List<Sequence> elements = factory.getVariableValues(symbol);
-						boolean elementMatches = false;
-						for (int k = 0; k < elements.size() && !elementMatches; k++) {
-							Sequence element = elements.get(k);
-							if (subSequence.startsWith(element)) {
-								//
-								ruleMatcher.addIndex(refIndex, k);
-								ruleMatcher.addVariable(refIndex, symbol);
-								ruleMatcher.addSequence(refIndex, element);
-								//
-								refIndex++;
-								testIndex += element.size();
-								elementMatches = true;
-							}
-						}
-						// If none of the variable elements match, fail
-						testIndex = elementMatches ? testIndex : -1;
-					} else if (segment.isUnderspecified()) {
-						// theoretically, this excludes fully specified features, but then
-						// why would use use bracket notation for that? just use the symbol
-
-						// Otherwise it's the same as a literal
-						if (subSequence.startsWith(segment)) {
-							ruleMatcher.addIndex(refIndex, -1);
-							ruleMatcher.addVariable(refIndex, symbol);
-							ruleMatcher.addSequence(refIndex, subSequence.subsequence(0, 1));
-							//
-							testIndex++;
-						} else {
-							testIndex = -1;
-						}
-					} else if (!symbol.equals("0")) {
-						testIndex = subSequence.startsWith(segment) ? testIndex + 1 : -1;
-					}
-				}
-
-				// This is checked second for a good reason: it may not be
-				// possible to know in advance what is the length of the
-				// matching initial until it's been evaluated, esp. in the
-				// case of a variable whose elements are allowed to have a
-				// length greater than 1. This is allowed because it is
-				// possible, or even likely, that a language might have a
-				// set of multi-segment clusters which still pattern
-				// together, or which are part of conditioning environments.
-				if (testIndex >= 0 && conditionsMatch(sequence, startIndex, testIndex)) {
-					index = testIndex;
-					// Now at this point, if everything worked, we can
-					Sequence removed= (startIndex < index)
-							? sequence.remove(startIndex, index)
-							: factory.getNewSequence();
-					Sequence replacement = getReplacementSequence(removed, target);
-					unmatched = false;
-					if (!replacement.isEmpty()) {
-						sequence.insert(replacement, startIndex);
-					}
-					index += replacement.size() - removed.size();
-					startIndex = index;
-				}
+		while (end < source.length()) {
+			char c = source.charAt(end);
+			if (c == ' ') {
+				list.add(source.substring(start, end));
+				end++;
+				start=end;
+			} else if (c == '[') {
+				end = source.indexOf(']', end);
+			} else {
+				end++;
 			}
 		}
-		if (unmatched) {
-			index++;
+		list.add(source.substring(start, end));
+		return list;
+	}
+
+	private static void balanceTransform(List<String> source, List<String> target, String transformation) {
+		if (target.size() > source.size()) {
+			throw new ParseException("Target size cannot be greater than " +
+					"source size.", transformation);
+		} else if (target.size() < source.size()) {
+			if (target.size() == 1) {
+				String first = target.get(0);
+				while (target.size() < source.size()) {
+					target.add(first);
+				}
+			} else {
+				throw new ParseException("Target and source sizes may only be" +
+						" uneven if target size is exactly 1.", transformation);
+			}
+		}
+	}
+
+	private int matchSource(Sequence sequence, Sequence source, int testIndex) {
+		int index = testIndex;
+		for (int i = 0; i < source.size() && index >= 0; i++) {
+			Sequence subSequence = sequence.subsequence(index);
+			Segment segment = source.get(i);
+
+			// Source symbol is a variable
+			String symbol = segment.getSymbol();
+			if (factory.hasVariable(symbol)) {
+				List<Sequence> elements = factory.getVariableValues(symbol);
+				boolean elementMatches = false;
+				for (int k = 0; k < elements.size() && !elementMatches; k++) {
+					Sequence element = elements.get(k);
+					if (subSequence.startsWith(element)) {
+						ruleMatcher.addIndex(k);
+						ruleMatcher.addVariable(symbol);
+						ruleMatcher.addSequence(element);
+						ruleMatcher.incrementIndex();
+						index += element.size();
+						elementMatches = true;
+					}
+				}
+				// If none of the variable elements match, fail
+				index = elementMatches ? index : -1;
+			} else if (segment.isUnderspecified()) {
+				// theoretically, this excludes fully specified features, but then
+				// why would use use bracket notation for that? just use the symbol
+
+				// Otherwise it's the same as a literal
+				if (subSequence.startsWith(segment)) {
+					ruleMatcher.addIndex(-1);
+					ruleMatcher.addVariable(symbol);
+					ruleMatcher.addSequence(subSequence.subsequence(0, 1));
+					index++;
+				} else {
+					index = -1;
+				}
+			} else if (!symbol.equals("0")) {
+				index =
+					  subSequence.startsWith(segment) ? index + 1 : -1;
+			}
 		}
 		return index;
 	}
@@ -250,23 +303,6 @@ public class BaseRule implements Rule {
 		}
 	}
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (!(o instanceof BaseRule)) return false;
-		BaseRule baseRule = (BaseRule) o;
-		return Objects.equals(ruleText, baseRule.ruleText) 
-				       && Objects.equals(conditions, baseRule.conditions) 
-				       && Objects.equals(exceptions, baseRule.exceptions)
-				       && Objects.equals(factory, baseRule.factory)
-				       && Objects.equals(transform, baseRule.transform);
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(ruleText, conditions, exceptions, factory, transform);
-	}
-
 	/**
 	 * Generates an appropriate sequence by filling in backreferences based on
 	 * the provided maps.
@@ -277,7 +313,7 @@ public class BaseRule implements Rule {
 	 * @return a Sequence object with variables and references filled in
 	 * according to the provided maps
 	 */
-	private Sequence getReplacementSequence(Sequence source, Sequence target) {
+	private Sequence getReplacement(Sequence source, Sequence target) {
 		int variableIndex = 1;
 		Sequence replacement = factory.getNewSequence();
 		// Step through the target pattern
@@ -288,7 +324,7 @@ public class BaseRule implements Rule {
 			Matcher matcher = BACKREFERENCE.matcher(symbol);
 			
 			if (matcher.matches()) {
-				Sequence sequence = getReferencedReplacement(target, matcher);
+				Sequence sequence = getReference(target, matcher);
 				replacement.add(sequence);
 			} else if (factory.hasVariable(symbol)) {
 				// Allows C > G transformations, where C and G have the same
@@ -304,10 +340,8 @@ public class BaseRule implements Rule {
 				FeatureArray<Double> features = alter.getFeatures();
 				FeatureModel model = factory.getFeatureModel();
 				String bestSymbol = model.getBestSymbol(features);
-				Segment newSegment = new Segment(
-						bestSymbol,
-						features,
-						model.getSpecification());
+				FeatureSpecification spec = model.getSpecification();
+				Segment newSegment = new Segment(bestSymbol, features, spec);
 				replacement.add(newSegment);
 			} else if (!symbol.equals("0")) {
 				// Normal segment and not 0
@@ -318,10 +352,8 @@ public class BaseRule implements Rule {
 		return replacement;
 	}
 
-	private Sequence getReferencedReplacement(
-			Sequence target,
-			MatchResult matcher
-	) {
+	// Referent?
+	private Sequence getReference(Sequence target, MatchResult matcher) {
 		String symbol = matcher.group(1);
 		String digits = matcher.group(2);
 
@@ -355,7 +387,6 @@ public class BaseRule implements Rule {
 		Iterator<Condition> eI = exceptions.iterator();
 
 		boolean conditionMatch = false;
-		boolean exceptionMatch = false;
 
 		if (cI.hasNext()) {
 			while (cI.hasNext() && !conditionMatch) {
@@ -366,6 +397,7 @@ public class BaseRule implements Rule {
 			conditionMatch = true;
 		}
 
+		boolean exceptionMatch = false;
 		if (eI.hasNext()) {
 			while (eI.hasNext() && !exceptionMatch) {
 				Condition exception = eI.next();
@@ -433,84 +465,51 @@ public class BaseRule implements Rule {
 			j++;
 		}
 	}
-
-	private static List<String> parseToList(String source) {
-		List<String> list = new ArrayList<>();
-		int start = 0;
-		int end   = 0;
-
-		while (end < source.length()) {
-			char c = source.charAt(end);
-			if (c == ' ') {
-				list.add(source.substring(start, end));
-				end++;
-				start=end;
-			} else if (c == '[') {
-				end = source.indexOf(']', end);
-			} else {
-				end++;
-			}
-		}
-		list.add(source.substring(start, end));
-		return list;
-	}
-
-	private static void balanceTransform(List<String> source, List<String> target, String transformation) {
-		if (target.size() > source.size()) {
-			throw new ParseException("Target size cannot be greater than " +
-					"source size.", transformation);
-		} else if (target.size() < source.size()) {
-			if (target.size() == 1) {
-				String first = target.get(0);
-				while (target.size() < source.size()) {
-					target.add(first);
-				}
-			} else {
-				throw new ParseException("Target and source sizes may only be" +
-						" uneven if target size is exactly 1.", transformation);
-			}
-		}
-	}
 	
 	private static final class RuleMatcher {
-		// These map from reference index ($) to:
-		
-		/*  Tracks the order of variables in the "source"
-		 *  pattern; i.e. the 2nd variable in the source pattern is referenced
-		 *  via {@code $2}. Unlike standard regular expressions, all variables
-		 *  are tracked, rather than tracking explicit groups */
-		private final Map<Integer, Integer>  indexMap;
-		
 		/* Tracks which variable values are matched by the "source" pattern; 
 		 * an entry (2 -> 4) would indicate that the source matched the 4th 
 		 * value of the 2nd variable. This permits proper mapping between source
 		 * and target symbols when using backreferences and indexed variables */
+		private final Map<Integer, Integer>  indexMap;
+		/* Track which variable in the rule was matched, by symbol */
 		private final Map<Integer, String>   variableMap;
-		
-		private final Map<Integer, Sequence> sequenceMap; // the actual input that was matched
+		/* The actual sequence matched in the input  */
+		private final Map<Integer, Sequence> sequenceMap;
+		/* Tracks the order of variables in the "source"
+		 * pattern; i.e. the 2nd variable in the source pattern is referenced
+		 * via {@code $2}. Unlike standard regular expressions, all variables
+		 * are tracked, rather than tracking explicit groups */
+		private int referenceIndex;
 
 		private RuleMatcher() {
+			referenceIndex = 1;
 			indexMap = new HashMap<>();
 			variableMap = new HashMap<>();
 			sequenceMap = new HashMap<>();
 		}
-		
+
+		public void incrementIndex() {
+			referenceIndex++;
+		}
+
 		private void reset() {
+			referenceIndex = 1;
 			indexMap.clear();
 			variableMap.clear();
 			sequenceMap.clear();
 		}
 		
-		private void addIndex(Integer i, Integer index) {
-			indexMap.put(i ,index);
+		private void addIndex(Integer index) {
+			indexMap.put(referenceIndex ,index);
 		}
 		
-		private void addVariable(Integer i, String variable) {
-			variableMap.put(i, variable);
+		private void addVariable(String variable) {
+			variableMap.put(referenceIndex, variable);
 		}
 		
-		private void addSequence(Integer i, Sequence sequence) {
-			sequenceMap.put(i, sequence);
+		private void addSequence(Sequence sequence) {
+			sequenceMap.put(referenceIndex, sequence);
 		}
 		
 		private Integer getIndex(Integer i) {
